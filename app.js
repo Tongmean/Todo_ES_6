26f3770ad6ec9cbf93b3f3f7663c3d921e889fab
Tongmean Teang <tongmean.teang@wdc.com>
2022-10-11 10:33:29 +0700

    Finish

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 const generateTemplate = (todo)=>{
     const html = `
-        <li class="list-group-item d-flex justify-content-between align-items-center" >
+        <li class="list-group-item" >
             <span>${todo}</span>
             <i class="fa fa-trash delete"></i>
         </li>
@@ -22,4 +23,29 @@ list.addEventListener('click', e =>{
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     };
+});
+
+
+const filterTodos = (term)=>{
+    Array.from(list.children)
+    .filter((todo)=>{
+        return !todo.textContent.toLowerCase().includes(term);
+    })
+    .forEach((todo)=>{
+        return todo.classList.add('filtered');
+    })
+
+    Array.from(list.children)
+    .filter((todo)=>{
+        return todo.textContent.toLowerCase().includes(term);
+    })
+    .forEach((todo)=>{
+        return todo.classList.remove('filtered');
+    })
+};
+
+// key event
+search.addEventListener('keyup', ()=>{
+    const term =search.value.trim().toLowerCase();
+    filterTodos(term);
 });
